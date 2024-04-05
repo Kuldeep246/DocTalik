cfd07b660a655a0c83bf28174394d781f05e93de
kuldeep246 <kuldeepgurjar2468@gmail.com>
2024-04-06 04:39:32 +0530

    pdf uploded

---
+++ src/components/PdfRenderer.tsx
@@ -0,0 +1,8 @@
+const PdfRenderer =()=>{
+    return (
+        <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+            <div className=""></div>
+        </div>
+    )
+}
+export default PdfRenderer
